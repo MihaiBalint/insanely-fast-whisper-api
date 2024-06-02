6e3ff1a0f41981671bc7481a322032e85acf6458
Mihai Balint <balint.mihai@gmail.com>
2024-06-02 08:45:32 +0300

    feat: enable dowloading for

--- app/diarization_pipeline.py
+++ app/diarization_pipeline.py
@@ -8,13 +8,17 @@ from .diarize import (
 )
 
 
-def diarize(hf_token, file_name, outputs):
+def build_pipeline(hf_token):
     diarization_pipeline = Pipeline.from_pretrained(
         checkpoint_path="pyannote/speaker-diarization-3.1",
         use_auth_token=hf_token,
     )
     diarization_pipeline.to(torch.device("cuda:0"))
+    return diarization_pipeline
 
+
+def diarize(hf_token, file_name, outputs):
+    diarization_pipeline = build_pipeline(hf_token)
     inputs, diarizer_inputs = preprocess_inputs(inputs=file_name)
 
     segments = diarize_audio(diarizer_inputs, diarization_pipeline)
--- app/download_weights.py
+++ app/download_weights.py
@@ -1,6 +1,9 @@
+from .diarization_pipeline import build_pipeline
+
 def main():
     print("Loading weights")
-    from .app import pipe
+    from .app import pipe, hf_token
+    build_pipeline(hf_token)
     print(f"Loaded {pipe.type}")
 
 if __name__ == "__main__":
